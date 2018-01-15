47f09f19ac6b729299111538c9f8b22d2b7d2784
dmsps <34230555+dmsps@users.noreply.github.com>
2018-01-15 12:31:07 +0300

    V3 Catalog, popup, social

--- js/app.js
+++ js/app.js
@@ -218,42 +218,16 @@ $(document).ready(function(){
   function initPopups(){
     // Magnific Popup
     // var startWindowScroll = 0;
-    $('.js-popup').magnificPopup({
-      type: 'inline',
-      fixedContentPos: true,
-      fixedBgPos: true,
-      overflowY: 'auto',
-      closeBtnInside: true,
+    $('.promo-video__link').magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
       preloader: false,
-      midClick: true,
-      removalDelay: 300,
-      mainClass: 'popup-buble',
-      callbacks: {
-        beforeOpen: function() {
-          // startWindowScroll = _window.scrollTop();
-          // $('html').addClass('mfp-helper');
-        },
-        close: function() {
-          // $('html').removeClass('mfp-helper');
-          // _window.scrollTop(startWindowScroll);
-        }
-      }
+  
+      fixedContentPos: true
     });
 
-    $('[js-popup-gallery]').magnificPopup({
-  		delegate: 'a',
-  		type: 'image',
-  		tLoading: 'Загрузка #%curr%...',
-  		mainClass: 'popup-buble',
-  		gallery: {
-  			enabled: true,
-  			navigateByImgClick: true,
-  			preload: [0,1]
-  		},
-  		image: {
-  			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-  		}
-  	});
   }
 
 
